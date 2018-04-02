e0abe3e1347088af12a81e785577fc7d336fc861
themikep82 <themikep@gmail.com>
2018-04-02 14:43:55 -0700

    daily cron job to get yesterdays statcast pitch events

---
+++ statcast/statcast_pitch_events_daily_cron.py
@@ -0,0 +1,99 @@
+import sys
+import requests
+import io
+import os
+from time import sleep
+from datetime import datetime, timedelta
+from slackclient import SlackClient
+sys.path.append("..")
+
+from dbhelper import *
+
+SLACK_ENABLED=1
+
+QUERY_DATE=datetime.today() + timedelta(days=-1)
+
+def main():
+
+	if SLACK_ENABLED:
+		sc = SlackClient(credentials['slackOAuthToken'])
+		sc.api_call(
+			"chat.postMessage",
+			channel="#dataimport",
+			text="Data Import Started!")
+		
+	dateWindow=QUERY_DATE
+		
+	if dateWindow.month not in [1, 2, 12]: #ignore January, February, December
+
+		print(dateWindow.strftime('%Y-%m-%d'))
+		
+		try:
+	
+			pitchEventData = collectStatcastPitchEvents(dateWindow.strftime('%Y-%m-%d'))
+			
+			if not pitchEventData.empty:
+				
+				pitchEventData = cleanData(pitchEventData)
+			
+				loadDataToRedshift(pitchEventData, dateWindow.strftime('%Y-%m-%d'))
+				
+				os.remove('statcast_ready' + dateWindow.strftime('%Y-%m-%d') +'.csv')
+						
+		except ValueException as e:
+			
+			if SLACK_ENABLED:
+				sc.api_call(
+					"chat.postMessage",
+					channel="#dataimport",
+					text=str(e))
+		
+	if SLACK_ENABLED:
+		sc.api_call(
+			"chat.postMessage",
+			channel="#dataimport",
+			text="Data Import Complete!")
+
+def cleanData(pitchEventData):
+	
+	#remove deprecated data and large 'des' column
+	pitchEventData=pitchEventData.drop(['spin_dir','spin_rate_deprecated','break_angle_deprecated','break_length_deprecated','des','tfs_deprecated','tfs_zulu_deprecated'], axis=1)
+	
+	#fix duplicate pos2_person_id column
+	pitchEventData=pitchEventData.drop(['pos2_person_id'], axis=1)
+	pitchEventData.rename(columns={'pos2_person_id.1':'pos2_person_id'}, inplace=True)
+	
+	#replace the word 'null' in data with actual null value
+	pitchEventData=pitchEventData.replace('null', '')
+	
+	#clean up decimal values on integer data
+	pitchEventData['on_3b']=dotZeroBugFix(pitchEventData['on_3b'])
+	pitchEventData['on_2b']=dotZeroBugFix(pitchEventData['on_2b'])
+	pitchEventData['on_1b']=dotZeroBugFix(pitchEventData['on_1b'])
+	
+	return pitchEventData
+	
+def loadDataToRedshift(pitchEventData, dateString):
+
+	#write sanatized data file to csv
+	pitchEventData.to_csv('statcast_ready' + dateString +'.csv', index=False)
+	
+	#move csv to S3 bucket
+	S3upload('statcast_ready' + dateString +'.csv','/statcast-historic/','blacksox-data')
+	
+	#Redshift SQL COPY command to load data from CSV into DB table
+	executeSQL("COPY statcast.pitch_events FROM 's3://blacksox-data/statcast-historic/statcast_ready" + dateString +".csv' CREDENTIALS 'aws_access_key_id=" + credentials["AWS_Key"] + ";aws_secret_access_key=" + credentials["AWS_Secret"] + "' IGNOREHEADER 1 DELIMITER AS ',';")
+
+def collectStatcastPitchEvents(dateString):
+	#modified from https://github.com/jldbc/pybaseball/blob/master/pybaseball/team_pitching.py
+
+	url = 'https://baseballsavant.mlb.com/statcast_search/csv?all=true&hfPT=&hfAB=&hfBBT=&hfPR=&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=R%7CPO%7CS%7C=&hfSea=&hfSit=&player_type=pitcher&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt={}&game_date_lt={}&team=&position=&hfRO=&home_road=&hfFlag=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=pitches&player_event_sort=h_launch_speed&sort_order=desc&min_abs=0&type=details&'.format(dateString, dateString)
+	
+	s=requests.get(url).content
+	df = pandas.read_csv(io.StringIO(s.decode('utf-8')))
+	df.head()
+		
+	return df
+	
+if __name__ == "__main__":
+	main()
